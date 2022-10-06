f163208ebb3b6e826df4f217e6ad6b8768ff51cf
Rocco Carpi <89945562+TheOfficialDragone@users.noreply.github.com>
2022-10-06 14:29:27 +0200

    feat: primi campi costruzione db

--- init-db.sql
+++ init-db.sql
@@ -42,4 +42,8 @@ CREATE TABLE account(
   data_nascita date NOT NULL,
   telefono text NOT NULL,
   TipoAccount int(10) NOT NULL
- )
+ );
+
+INSERT INTO account(password, nome, cognome, email, indirizzo, data_nascita, telefono, TipoAccount)
+VALUES ('1', 'Admin', 'Admin', 'admin@test.local', 'parma', '2000/01/01', '1234567890', '1'),
+('test', 'magazziniere', 'test', 'magazziniere@test.local', 'parma', '2002/12/12', '1234567890', '2')
